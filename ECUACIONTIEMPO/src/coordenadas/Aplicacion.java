e22fe654bc80a9d8aa566c67bd06ff01dfc6c8b3
Edu <xclimb@gmail.com>
2019-03-04 20:50:07 +0100

    Commit nº2: eliminé unos comentarios del código en la clase Apliacion

--- ECUACIONTIEMPO/src/coordenadas/Aplicacion.java
+++ ECUACIONTIEMPO/src/coordenadas/Aplicacion.java
@@ -6,20 +6,6 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		
-//		int grados = Integer.parseInt(JOptionPane.showInputDialog
-//				("Escribe los grados"));
-//		
-//		int minutos = Integer.parseInt(JOptionPane.showInputDialog
-//				("Escribe los minutos"));
-//		
-//		int segundos = Integer.parseInt(JOptionPane.showInputDialog
-//				("Escribe los segundos"));
-//		
-//		Coordenadas coordenadas1 = new Coordenadas(grados,minutos,segundos);
-//		
-//		JOptionPane.showMessageDialog(null, "Sus coordenadas en grados"
-//				+ " son " + coordenadas1.convertirAGrados());
-		
 		DiaDelAño nuevoDia = new DiaDelAño(4,3,2019);
 		
 		System.out.println(nuevoDia.esBisiesto());
