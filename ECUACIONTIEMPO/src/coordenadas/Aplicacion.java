b31a95903cbb3cab8164e281bc5007111cc32719
Edu <xclimb@gmail.com>
2019-03-18 18:39:13 +0100

    Añadí comentarios en la clase Aplicacion

--- ECUACIONTIEMPO/src/coordenadas/Aplicacion.java
+++ ECUACIONTIEMPO/src/coordenadas/Aplicacion.java
@@ -12,7 +12,9 @@ public class Aplicacion {
 		System.out.println(nuevoDia.getDiaDelAño());
 		System.out.println(nuevoDia.getAnguloFromDia());
 		System.out.println(nuevoDia.getEcuacionTiempo());
-
+		
+		/*Me voy a cagar en todo a ver si esto me sale de una vez*/
+		/*Que está pasando por que no va*/
 	}
 
 }
